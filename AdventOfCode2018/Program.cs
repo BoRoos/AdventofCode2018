fdd46c71ff65e91468810fcae22f096f5a5ca7ef
Bo Roos <bo.roos@cgi.com>
2018-12-05 07:39:11 +0100

    d5

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -13,9 +13,68 @@ namespace AdventOfCode2018
             //Day1();
             //Day2();
             //Day3();
-            Day4();
+            //Day4();
+            Day5();
         }
 
+        static void Day5()
+        {
+            var data = File.ReadAllLines(@"c:\temp\input.txt");
+            var charlist = data[0].ToCharArray().ToList();
+            var runagain = false;
+            //do
+            //{
+            //    runagain = false;
+            //    for (var i = 0; i < charlist.Count - 1; i++)
+            //    {
+            //        if (Char.ToLower(charlist[i]) == Char.ToLower(charlist[i + 1]) && (int)charlist[i] != (int)charlist[i + 1])
+            //        {
+            //            charlist.RemoveAt(i);
+            //            charlist.RemoveAt(i);
+            //            runagain = true;
+            //            break;
+            //        }
+            //    }
+
+            //} while (runagain);
+
+            Console.WriteLine(charlist.Count);
+            var filterlist = "abcdefghijklmnopqrstuvwxyz";
+            var currentcount = 100000;
+            var currentlist = new List<string>();
+            foreach(var letter in filterlist)
+            {
+                var d = data[0].Replace(letter.ToString(), "").Replace(Char.ToUpper(letter).ToString(),"");
+                charlist = d.ToCharArray().ToList();
+                runagain = false;
+                do
+                {
+                    runagain = false;
+                    for (var i = 0; i < charlist.Count - 1; i++)
+                    {
+                        if (Char.ToLower(charlist[i]) == Char.ToLower(charlist[i + 1]) && (int)charlist[i] != (int)charlist[i + 1])
+                        {
+                            charlist.RemoveAt(i);
+                            charlist.RemoveAt(i);
+                            runagain = true;
+                            break;
+                        }
+                    }
+
+                } while (runagain);
+
+
+                if (charlist.Count < currentcount)
+                {
+                    currentcount = charlist.Count;
+                    currentlist = charlist.Select(c => c.ToString()).ToList();
+                }
+            }
+
+            Console.WriteLine(currentlist.Count);
+
+            Console.ReadKey();
+        }
         static void Day4()
         {
             var data = File.ReadAllLines(@"c:\temp\input.txt");
