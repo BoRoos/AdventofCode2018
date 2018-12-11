08dcffd0851b99b74f26c5fe9e1165c028e71d6d
Bo Roos <bo.roos@cgi.com>
2018-12-11 06:56:20 +0100

    11

--- AdventOfCode2018/Day11.cs
+++ AdventOfCode2018/Day11.cs
@@ -8,11 +8,58 @@ namespace AdventOfCode2018
     class Day11
     {
         public static void Execute()
-        {
-            char[] pattern = { '<', '>', ','};
-            var data = File.ReadAllLines(@"c:\temp\input11.txt");
-       
-            Console.WriteLine("");
+        { //9810
+            var serialnumber = 9810;
+            var loopsize = 300;
+            var grid = new int[loopsize+1, loopsize+1];
+            for (var x =1; x<loopsize; x++ )
+            {
+                for (var y = 1; y < loopsize; y++)
+                {
+                    var rack = x + 10;
+                    var powerlevel = rack * y;
+                    var fuel = powerlevel + serialnumber;
+                    fuel *= rack;
+                    grid[x, y] = fuel / 100%10 -5;
+
+                }
+            }
+
+            var currentfuel = 0;
+            var currentx = 0;
+            var currenty = 0;
+            var currentsquare = 0;
+            for (var square = 1; square < 300; square++)
+            {
+                for (var x = 1; x < loopsize - square; x++)
+                {
+                    for (var y = 1; y < loopsize - square; y++)
+                    {
+                        var fuel = 0;
+
+
+                        for (var i = 0; i < square; i++)
+                        {
+                            for (var j = 0; j < square; j++)
+                            {
+                                fuel += grid[x + i, y + j];
+                            }
+                        }
+                        if (fuel > currentfuel)
+                        {
+                            currentfuel = fuel;
+                            currentx = x;
+                            currenty = y;
+                            currentsquare = square;
+                            Console.WriteLine(currentx + "," + currenty + "," + currentsquare + " " + currentfuel);
+                        }
+
+                    }
+                }
+            }
+
+
+            Console.WriteLine(currentx + "," + currenty +"," +currentsquare + " " + currentfuel );
             Console.ReadKey();
 
         }
